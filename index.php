5458bf32539cc45aa548051b5705288b82873cd2
sparc64 <sparc64@riseup.net>
2017-07-17 21:19:48 -0400

    Converted data to JSON and saved as js variable for highcharts

--- index.php
+++ index.php
@@ -9,6 +9,36 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha256-rr9hHBQ43H7HSOmmNkxzQGazS/Khx+L8ZRHteEY1tQ4=" crossorigin="anonymous" />
 </head>
 <body>
+  
+  <div class="container mt-5">
+    
+		<div class="row">
+			<div class="col">
+				<h1 class="font-weight-bold float-left">Basic Participants</h1>
+			</div>
+		</div>
+		
+    <div class="row">
+      <div class="col">
+        <h2>Attendance and Delivery</h2>
+      </div>
+    </div>
+    
+		<div class="row">
+			<div class="col">
+        
+        <table class="table table-bordered">
+          <thead>
+            <tr><th>Attendance</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>Table row</td></tr>
+          </tbody>
+        </table>
+			</div>
+		</div>
+    
+	</div>
  
   <?php
     $handle = fopen("data/Basic Participants.csv", "r");
@@ -26,7 +56,13 @@
     print_r($dataArray);
     echo '</pre>';
   
+    $jsonData = json_encode($dataArray, 5);
+  
+    echo "<script>const stringData = '$jsonData'</script>";
+  
   ?>
+  
+  
 
 	<!-- 	
 	Using CDNJS for speed and built-in Subresource Integrity (SRI)
