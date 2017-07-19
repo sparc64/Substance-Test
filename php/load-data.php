d992576d57284268006937f557dd64642361d4e4
sparc64 <sparc64@riseup.net>
2017-07-19 01:52:30 -0400

    Removed log functions and unnecessary comments

--- php/load-data.php
+++ php/load-data.php
@@ -7,12 +7,7 @@
       // Too complex but don't know how to make it more readable preserving same efficiency
       $dataArray[ $data[0] ][ $data[1] ][ $data[2] ][] = [ "description" => $data[3], "breakdown" => $data[4], "value" => $data[5], "percentage" => $data[6] ];
         
-        //array_slice($data, 3) ;
     }
     fclose($handle);
   }
-
-//   echo '<pre>';
-//   print_r($dataArray);
-//   echo '</pre>';
 ?>
